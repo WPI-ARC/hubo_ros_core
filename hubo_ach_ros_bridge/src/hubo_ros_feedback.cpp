a3adbb71d68e3abe1298910abccf7c63e460a642
Calder Phillips-Grafflin <calderpg@sabine.(none)>
2013-07-10 12:18:07 -0400

    Fixed parameter configs for both drchubo and huboplus, added new fields to joint command message to gather error data from hubo ach, added support in the feedback bridge for new message fields, and added data for existing fields

--- hubo_ach_ros_bridge/src/hubo_ros_feedback.cpp
+++ hubo_ach_ros_bridge/src/hubo_ros_feedback.cpp
@@ -54,6 +54,9 @@ Copyright (c) 2012, Daniel M. Lofaro
 // ACH channel
 ach_channel_t chan_hubo_state;
 
+// Clock storage
+double g_last_clock;
+
 // ROS publishers
 ros::Publisher g_hubo_state_pub;
 ros::Publisher g_hubo_clock_pub;
@@ -83,34 +86,54 @@ bool ACHtoHuboState(struct hubo_state * robot_state)
 {
     if (robot_state != NULL)
     {
-        //Publish clock info
+        // Publish clock info
         rosgraph_msgs::Clock clock_state;
         clock_state.clock = ros::Time(robot_state->time);
         g_hubo_clock_pub.publish(clock_state);
-        //Make new message
+        // Make new message
         hubo_robot_msgs::JointCommandState joint_msg;
-        //Read through the two Hubo-ACH structs into the joint_message
+        // Read through the two Hubo-ACH structs into the joint_message
         for (int i = 0; i < HUBO_JOINT_COUNT; i++)
         {
             if (robot_state->joint != NULL)
             {
-                //Copy an individual joint
+                // Copy an individual joint
                 hubo_robot_msgs::JointControllerState joint_state;
+                // Populate joint status (what it's doing, not how it's doing)
                 joint_state.joint_name = IndexToJointName(i);
                 joint_state.set_point = robot_state->joint[i].ref;
                 joint_state.process_value = robot_state->joint[i].pos;
                 joint_state.process_value_dot = robot_state->joint[i].vel;
                 joint_state.error = robot_state->joint[i].ref - robot_state->joint[i].pos;
-                joint_state.time_step = NAN;
-                joint_state.command = NAN;
+                joint_state.time_step = robot_state->time - g_last_clock;
+                joint_state.command = robot_state->joint[i].duty;
                 joint_state.p = NAN;
                 joint_state.i = NAN;
                 joint_state.d = NAN;
                 joint_state.i_clamp = NAN;
                 joint_state.current = robot_state->joint[i].cur;
                 joint_state.temperature = robot_state->joint[i].tmp;
-                joint_state.active = (int)robot_state->joint[i].active;
-                joint_state.zeroed = (int)robot_state->joint[i].zeroed;
+                // Populate joint status (how it's doing, not what it's doing)
+                joint_state.active = robot_state->joint[i].active;
+                joint_state.zeroed = robot_state->joint[i].zeroed;
+                joint_state.driver_on = robot_state->status[i].driverOn;
+                joint_state.controller_on = robot_state->status[i].ctrlOn;
+                joint_state.mode = robot_state->status[i].mode;
+                joint_state.limit_switch = robot_state->status[i].limitSwitch;
+                joint_state.home_flag = robot_state->status[i].homeFlag;
+                joint_state.jammed = robot_state->status[i].jam;
+                joint_state.pwm_saturated = robot_state->status[i].pwmSaturated;
+                joint_state.big_error = robot_state->status[i].bigError;
+                joint_state.encoder_error = robot_state->status[i].encError;
+                joint_state.driver_fault = robot_state->status[i].driverFault;
+                joint_state.motor_fail_0 = robot_state->status[i].motorFail0;
+                joint_state.motor_fail_1 = robot_state->status[i].motorFail1;
+                joint_state.min_position_error = robot_state->status[i].posMinError;
+                joint_state.max_position_error = robot_state->status[i].posMaxError;
+                joint_state.velocity_error = robot_state->status[i].velError;
+                joint_state.acceleration_error = robot_state->status[i].accError;
+                joint_state.temperature_error = robot_state->status[i].tempError;
+                // Store it
                 joint_msg.joint_names.push_back(joint_state.joint_name);
                 joint_msg.state.push_back(joint_state);
             }
@@ -119,9 +142,10 @@ bool ACHtoHuboState(struct hubo_state * robot_state)
                 ROS_ERROR("*** WARNING - NULL joint state received! ***\n");
             }
         }
-        joint_msg.header.frame_id = std::string("/hubo_base_link");
+        joint_msg.header.frame_id = std::string("");
         joint_msg.header.stamp = ros::Time::now();
         g_hubo_state_pub.publish(joint_msg);
+        g_last_clock = robot_state->time;
         return true;
     }
     else
@@ -139,6 +163,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     ros::NodeHandle nhp("~");
     ROS_INFO("Initializing ACH-to-ROS bridge [feedback]");
+    g_last_clock = 0.0;
     // Load joint names and mappings
     XmlRpc::XmlRpcValue joint_names;
     if (!nhp.getParam("joints", joint_names))
